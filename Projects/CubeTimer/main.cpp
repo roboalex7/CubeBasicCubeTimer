5aefd6191a94be1591edf2e150cf14e8e54cd529
roboalex7 <alex@gtvinc.com>
2017-02-23 10:47:33 -0500

    2x2 thru 7x7 scrambles now functioning.  Scramble class will need some serious refactoring DTL

--- Projects/CubeTimer/main.cpp
+++ Projects/CubeTimer/main.cpp
@@ -17,9 +17,12 @@ int main()
 
     beginning:
 
-    scrambler->Scramble2x2();
-    scrambler->Scramble3x3();
-    scrambler->Scramble4x4();
+//    scrambler->Scramble2x2();
+//    scrambler->Scramble3x3();
+//    scrambler->Scramble4x4();
+//    scrambler->Scramble5x5();
+        scrambler->Scramble6x6();
+        scrambler->Scramble7x7();
 
     timer->RunTimer();
 
--- Projects/CubeTimer/scramble.cpp
+++ Projects/CubeTimer/scramble.cpp
@@ -104,3 +104,124 @@ void Scrambler::Scramble4x4()
     }
     cout << endl;
 }
+
+
+
+void Scrambler::Scramble5x5()
+{
+    for (int i = 1; i <= 60; i = i + 1)
+    {
+        char oldch;
+
+        label:
+
+        char clut[] = { 'R','U','B','L','F','D',};
+        char ch = clut[rand() % sizeof(clut)];
+
+        if(ch == oldch)
+        goto label;
+
+        cout << ch;
+        //out << ch << " , " << oldch << endl;
+
+        oldch = ch;
+
+        char llut[] = {' ', 'w'};
+        char layer = llut[rand() % sizeof(llut)];
+        if (layer == 'w')
+            cout << layer;
+
+
+        char mlut[] = { '\'', ' ', '2' };
+        char mod = mlut[rand() % sizeof(mlut)];
+
+        cout << mod << " ";
+    }
+    cout << endl;
+}
+
+void Scrambler::Scramble6x6()
+{
+    for (int i = 1; i <= 80; i = i + 1)
+    {
+        char oldch;
+
+        label:
+
+        char clut[] = { 'R','U','B','L','F','D',};
+        char ch = clut[rand() % sizeof(clut)];
+
+        if(ch == oldch)
+        goto label;
+
+        //cout << ch;
+        //out << ch << " , " << oldch << endl;
+
+        oldch = ch;
+
+        char llut[] = {' ', 'w'};
+        char layer = llut[rand() % sizeof(llut)];
+        char mllut[] = {' ', '3'};
+        char middleLayer = mllut[rand() % sizeof(mllut)];
+
+        if (layer == 'w' && middleLayer == '3')
+            cout << "3" << ch << layer;
+        else if (layer == 'w')
+            cout << ch << layer;
+        else
+            cout << ch;
+
+
+
+
+        char mlut[] = { '\'', ' ', '2' };
+        char mod = mlut[rand() % sizeof(mlut)];
+
+        cout << mod << " ";
+    }
+    cout << endl;
+}
+
+
+
+void Scrambler::Scramble7x7()
+{
+    for (int i = 1; i <= 100; i = i + 1)
+    {
+        char oldch;
+
+        label:
+
+        char clut[] = { 'R','U','B','L','F','D',};
+        char ch = clut[rand() % sizeof(clut)];
+
+        if(ch == oldch)
+        goto label;
+
+        //cout << ch;
+        //out << ch << " , " << oldch << endl;
+
+        oldch = ch;
+
+        char llut[] = {' ', 'w'};
+        char layer = llut[rand() % sizeof(llut)];
+        char mllut[] = {' ', '3'};
+        char middleLayer = mllut[rand() % sizeof(mllut)];
+
+        if (layer == 'w' && middleLayer == '3')
+            cout << "3" << ch << layer;
+        else if (layer == 'w')
+            cout << ch << layer;
+        else
+            cout << ch;
+
+
+
+
+        char mlut[] = { '\'', ' ', '2' };
+        char mod = mlut[rand() % sizeof(mlut)];
+
+        cout << mod << " ";
+    }
+    cout << endl;
+}
